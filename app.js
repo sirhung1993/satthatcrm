684058a1d8cbe5eddddb15eaf64772eebf080cf2
hungbv2 <hungbv2@topica.edu.vn>
2019-03-28 18:49:37 +0700

    Fix / route

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
-
+app.get('/', (req, res, next) => {
+    res.status(200).json({data: 'Ok from server'})
+})
 module.exports = app;
